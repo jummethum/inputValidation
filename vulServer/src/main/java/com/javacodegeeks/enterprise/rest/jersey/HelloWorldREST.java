4997fd562506c34d7ad4592bf77df3e5394a93b1
Janek Ummethum <Janek.Ummethum@msg.group>
2018-08-27 13:17:20 +0200

    inital xss exploit example

--- vulServer/src/main/java/com/javacodegeeks/enterprise/rest/jersey/HelloWorldREST.java
+++ vulServer/src/main/java/com/javacodegeeks/enterprise/rest/jersey/HelloWorldREST.java
@@ -1,24 +1,27 @@
 package com.javacodegeeks.enterprise.rest.jersey;
 
-import javax.ws.rs.DefaultValue;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-@Path("/helloWorldREST")
+@Path("/vul")
 public class HelloWorldREST {
-
+	
 	@GET
-	@Path("/{parameter}")
-	public Response responseMsg( @PathParam("parameter") String parameter,
-			@DefaultValue("Nothing to say") @QueryParam("value") String value) {
-
-		String output = "Hello from: " + parameter + " : " + value;
-
+	@Path("/search")
+	public Response search(
+			@QueryParam("searchString")
+//		    @NotNull
+//		    @Size(min = 1, max = 255)
+//		    @Pattern(regexp = "\\d+")
+			String term) {
+		
+		String output = term + " not found. Please try a differnt search word.";
 		return Response.status(200).entity(output).build();
 
 	}
-
 }
